aa00de3abb27e2b50e6e3b400410c9cc58976801
MinKhantYCC <linyaungzin99@gmail.com>
2024-12-16 15:02:11 +0700

    Response ORM models

--- app/main.py
+++ app/main.py
@@ -6,46 +6,28 @@ from . import model, schemas
 from .database import get_db, engine
 from sqlalchemy.orm import Session
 from fastapi import Depends
-
-model.Base.metadata.create_all(bind=engine)
+from typing import List
 
 app = FastAPI()
-
-## database
-while True:
-    try:
-        conn = psycopg2.connect(
-            host="localhost", 
-            database='fastapi', 
-            user='postgres', 
-            password='min#25699',
-            cursor_factory=RealDictCursor,
-        )
-        cursor = conn.cursor()
-        print("Database connection was successful.")
-        break
-    except Exception as e:
-        print(f"Cannot connect to the database. Detail: {e}")
-        time.sleep(3)
 
 ## routes
 @app.get("/")
 def index():
     return {"message": "Hello World"}
 
-@app.get("/posts")
+@app.get("/posts", response_model=List[schemas.Post])
 def get_posts(db: Session = Depends(get_db)):
     posts = db.query(model.Post).all()
-    return {"data": posts}
+    return posts
 
-@app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_post(payload: schemas.Post, db:Session=Depends(get_db)):
+@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
+def create_post(payload: schemas.PostBase, db:Session=Depends(get_db)):
     new_post = model.Post(**payload.model_dump())
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
 
-    return {"data": new_post}
+    return new_post
 
 @app.get("/posts/{id}")
 def get_post(id:int, db:Session=Depends(get_db)):
@@ -55,10 +37,10 @@ def get_post(id:int, db:Session=Depends(get_db)):
             status_code=status.HTTP_404_NOT_FOUND, 
             detail=f"Post with id: {id} was not found."
         )
-    return {"post": post}
+    return post
 
-@app.put("/posts/{id}")
-def update_post(id:int, post: schemas.Post, db:Session=Depends(get_db)):
+@app.put("/posts/{id}", response_model=schemas.Post)
+def update_post(id:int, post: schemas.PostBase, db:Session=Depends(get_db)):
     post_query = db.query(model.Post).filter(model.Post.id == id)
     if post_query.first() == None:
         raise HTTPException(
@@ -68,7 +50,7 @@ def update_post(id:int, post: schemas.Post, db:Session=Depends(get_db)):
     post_query.update(post.model_dump(), synchronize_session=False)
     db.commit()
     db.refresh(post_query.first())
-    return {"data": post_query.first()}
+    return post_query.first()
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id:int, db:Session=Depends(get_db)):
--- app/schemas.py
+++ app/schemas.py
@@ -1,7 +1,15 @@
 from pydantic import BaseModel
+from datetime import datetime
 
 ## Pydantic Models
-class Post(BaseModel):
+class PostBase(BaseModel):
     title: str
     content: str
     published: bool = True
+
+class Post(PostBase):
+    id: int
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
